f1a8df3b2d9261e95727adb5b4453116a0531f90
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-03-14 14:44:56 -0400

    passing

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,34 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    # badge_messages = []
+    # for name in names:
+    #     badge_messages.append(f'Hello, my name is {name}.')
+    # return badge_messages
+   
+    return [f'Hello, my name is {name}.' for name in names]
 
 def assign_rooms(names):
-    return None
+    # room_assignments = []
+
+    # for i, name in enumerate(names, start = 1):
+    #     room_assignments.append(f"Hello, {name}! You'll be assigned to room {i}!")
+    # return room_assignments
+
+    room_assignments = [f"Hello, {name}! You'll be assigned to room {i}!" for i, name in enumerate(names, start = 1)]
+    return room_assignments
 
 def printer(names):
-    return None
+   badge_messages = batch_badge_creator(names)
+   for message in badge_messages:
+       print(message)
+
+
+   room_assignments = assign_rooms(names)
+   for assignment in room_assignments:
+       print(assignment)
+       
+
+
+        
